73062378e90c34a86ce81eeb5f2b26b4d65c74e3
KimMyeongJun <myeongjun222@naver.com>
2020-07-15 19:07:37 +0900

    ChatApp

--- app/src/main/java/com/example/messenger/LoginActivity.kt
+++ app/src/main/java/com/example/messenger/LoginActivity.kt
@@ -4,65 +4,154 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
-    var auth : FirebaseAuth? = null;
+    //파이어베이스
+    var auth : FirebaseAuth? = null
+    //구글 로그인 Client
+    var googleSignInClient : GoogleSignInClient? = null
+    var GOOGLE_LOGIN_CODE = 9001
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        auth = FirebaseAuth.getInstance();
+        auth = FirebaseAuth.getInstance()
 
-    }
-    //회원 가입을 위한 Function
-    fun signinAndSignup(){
-        auth?.createUserWithEmailAndPassword(
-            email_edittext.text.toString(),
-            password_edittext.text.toString()
-        )?.addOnCompleteListener{
-            task -> if(task.isSuccessful) {
-                //회원가입 성공
-                moveMainPage(task.result.user)
-            }else if(!task.exception?.message.isNullOrEmpty()){
-                //회원가입 실패
+        //로그인 버튼 클릭시  SinginEmail Function Call
+        email_login.setOnClickListener {
+            if (email_edittext.text.toString() != "" && password_edittext.text.toString() != "") {
+                //ID PW
+                SinginEmail()
+            } else {
                 Toast.makeText(
                     this,
-                    task.exception?.message,
+                    "ID와 Password를 입력하세요",
                     Toast.LENGTH_LONG
                 ).show()
+            }
+        }
+
+        //회원 가입 버튼 클릭시 MoveSignupPage Function Call
+        signup.setOnClickListener{
+            MoveSignupPage()
+        }
+
+        //구글 로그인 옵션
+        var gso = GoogleSignInOptions.Builder(
+            GoogleSignInOptions.DEFAULT_SIGN_IN
+        )
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        //옵션값을 GoogleSignInClient에 입력
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        //구글로 로그인하는 Button 클릭시 GoogleLogin Function Call
+        google_signin_button.setOnClickListener{
+            GoogleLogin()
+        }
+    }
+
+    //구글 Login을 위한 Function
+    fun GoogleLogin(){
+        var signInIntent = googleSignInClient?.signInIntent
+        //구글로부터 ID/PW값을 받아오기 위함
+        startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
+    }
+
+    //구글의 Data를 가져오는 override Function
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == GOOGLE_LOGIN_CODE){
+            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if (result != null) {
+                if(result.isSuccess){
+                    var account = result.signInAccount
+                    FirebaseGoogleAuth(account)
+                }
             }else{
-                //회원가입과 에러가 아닐 경우 -> 로그인 화면으로 이동
-                singinEmail()
+                Toast.makeText(
+                    this,
+                    "구글 로그인 실패",
+                    Toast.LENGTH_LONG
+                ).show()
             }
         }
     }
 
+    fun FirebaseGoogleAuth(account : GoogleSignInAccount?){
+        var credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener{
+                task -> if(task.isSuccessful) {
+                    //ID Password가 일치할 경우, MainPage로 이동하는 Function 호출
+                    MoveMainPage(task.result?.user)
+                }else{
+                    //ID Password가 일치하지 않을 경우, 실패 Message 출력
+                    Toast.makeText(this,
+                        "ID와 PW가 맞지 않습니다. 다시 입력해주세요",
+                        Toast.LENGTH_LONG
+                    ).show()
+
+                    Toast.makeText(
+                        this,
+                        task.exception?.message,
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+        }
+    }
+
     //Login을 위한 Function
-    fun singinEmail(){
+    fun SinginEmail(){
         auth?.signInWithEmailAndPassword(
             email_edittext.text.toString(),
             password_edittext.text.toString()
         )?.addOnCompleteListener{
             task -> if(task.isSuccessful) {
-                //ID Password가 일치할 경우
-
+                //ID Password가 일치할 경우, MainPage로 이동하는 Function 호출
+                MoveMainPage(task.result?.user)
             }else{
-                //ID Password가 일치하지 않을 경우
+                //ID Password가 일치하지 않을 경우, 실패 Message 출력
+                Toast.makeText(this,
+                    "ID와 PW가 맞지 않습니다. 다시 입력해주세요",
+                    Toast.LENGTH_LONG
+                ).show()
 
+                Toast.makeText(
+                    this,
+                    task.exception?.message,
+                    Toast.LENGTH_LONG
+                ).show()
             }
         }
     }
+
     //로그인 성공 후 MainPage로 이동하는 Function
-    fun moveMainPage(user : FirebaseUser?){
+    fun MoveMainPage(user : FirebaseUser?){
         if(user != null){
             startActivity(Intent(
-                this,
+                this@LoginActivity,
                 MainActivity::class.java)
             )
         }
     }
 
+    //회원가입 페이지로 이동
+    fun MoveSignupPage(){
+        startActivity(Intent(
+            this@LoginActivity,
+            SignUp::class.java))
+    }
 }
---
+++ app/src/main/java/com/example/messenger/SignUp.kt
@@ -0,0 +1,71 @@
+package com.example.messenger
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_sign_up.*
+
+class SignUp : AppCompatActivity() {
+    var auth : FirebaseAuth? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        auth = FirebaseAuth.getInstance()
+
+        signup_button.setOnClickListener{
+            Signup()
+        }
+        signup_before.setOnClickListener{
+            MoveSignupPage()
+        }
+    }
+
+    //회원 가입을 위한 Function
+    fun Signup(){
+        auth?.createUserWithEmailAndPassword(
+            signup_email.text.toString(),
+            signup_password.text.toString()
+            )?.addOnCompleteListener{
+                    task -> if(task.isSuccessful) {
+                //회원가입 성공
+                MoveMainPage(task.result?.user)
+            }else if(!task.exception?.message.isNullOrEmpty()){
+                //회원가입 실패
+                Toast.makeText(
+                    this,
+                    task.exception?.message,
+                    Toast.LENGTH_LONG
+                ).show()
+            }else{
+                //회원가입과 에러가 아닐 경우, 로그인 화면으로 이동
+                //Login Function
+            startActivity(
+                Intent(
+                    this,
+                    LoginActivity::class.java)
+                )
+            }
+        }
+    }
+
+    //회원가입 성공 후 Main페이지로 이동
+    fun MoveMainPage(user : FirebaseUser?){
+        if(user != null){
+            startActivity(Intent(
+                this,
+                MainActivity::class.java)
+            )
+        }
+    }
+    //회원가입 페이지로 이동
+    fun MoveSignupPage(){
+        startActivity(Intent(
+            this,
+            LoginActivity::class.java))
+    }
+}
